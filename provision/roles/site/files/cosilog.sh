3aa8f1305aec70c312a811fa491e8b6eb8ca2aad
matt maier <maier@users.noreply.github.com>
2016-05-20 12:16:24 -0400

    switch to tailing cosi-site.log

--- provision/roles/site/files/cosilog.sh
+++ provision/roles/site/files/cosilog.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-export PATH="/opt/node/bin:/opt/circonus/osi-site/node_modules/bunyan/bin:$PATH"
-journalctl -f -u cosi-site -o cat | bunyan
+
+node_path="/opt/node/bin"
+[[ -d $node_path ]] || node_path="/opt/circonus/bin"
+export PATH="$node_path:/opt/circonus/osi-site/node_modules/bunyan/bin:$PATH"
+
+cosi_log="/opt/circonus/osi-site/log/cosi-site.log"
+if [[ -f $cosi_log ]]; then
+    tail -f $cosi_log | bunyan
+else 
+    journalctl -f -u cosi-site -o cat | bunyan
+fi
