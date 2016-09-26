87f5630d03670cd4d8547d0ca809964bf0a44b54
matt maier <maier@users.noreply.github.com>
2016-09-26 11:23:12 -0400

    lint

--- src/util/lib/cosi/template/index.js
+++ src/util/lib/cosi/template/index.js
@@ -1,23 +1,25 @@
-"use strict";
+'use strict';
 
-/*eslint-env node, es6 */
-/*eslint-disable no-magic-numbers, no-process-exit */
+/* eslint-env node, es6 */
+/* eslint-disable no-magic-numbers, no-process-exit */
 
-const assert = require("assert");
-const path = require("path");
-const fs = require("fs");
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
 
-const chalk = require("chalk");
+const chalk = require('chalk');
+
+const cosi = require(path.resolve(path.join(__dirname, '..')));
 
 function templatePropertyFilter(key, value) {
-    if (typeof value === "function") {
-        return undefined;   //eslint-disable-line no-undefined
+    if (typeof value === 'function') {
+        return undefined;   // eslint-disable-line no-undefined
     }
-    if (key === "id") {     // created when parsed/loaded, ignore
-        return undefined;   //eslint-disable-line no-undefined
+    if (key === 'id') {     // created when parsed/loaded, ignore
+        return undefined;   // eslint-disable-line no-undefined
     }
-    if (key === "type") {   // created when parsed/loaded, ignore
-        return undefined;   //eslint-disable-line no-undefined
+    if (key === 'type') {   // created when parsed/loaded, ignore
+        return undefined;   // eslint-disable-line no-undefined
     }
     return value;
 }
@@ -26,11 +28,10 @@ module.exports = class Template {
     constructor(src) {
         // convienence constructor (pass json string or file spec)
         if (src) {
-            assert.strictEqual(typeof src, "string", "optional argument src, must be a string");
-            if (src.substr(0, 1) === "{") { // smells like json
+            assert.strictEqual(typeof src, 'string', 'optional argument src, must be a string');
+            if (src.substr(0, 1) === '{') { // smells like json
                 this.parse(src);
-            }
-            else {
+            } else {
                 this.load(src); // ASSume it's a file
             }
         }
@@ -41,22 +42,21 @@ module.exports = class Template {
     }
 
     load(fileName) {
-        assert.strictEqual(typeof fileName, "string", "fileName is required");
+        assert.strictEqual(typeof fileName, 'string', 'fileName is required');
 
         let data = null;
 
         try {
-            data = fs.readFileSync(fileName, { encoding: "utf8" });
-        }
-        catch (err) {
-            const msg = chalk.red("Load template:");
+            data = fs.readFileSync(fileName, { encoding: 'utf8' });
+        } catch (err) {
+            const msg = chalk.red('Load template:');
 
-            if (err.code === "ENOENT") {
+            if (err.code === 'ENOENT') {
                 console.error(msg, err.toString());
                 process.exit(1);
             }
 
-            if (err.code === "EACCES") {
+            if (err.code === 'EACCES') {
                 console.error(msg, err.toString());
                 process.exit(1);
             }
@@ -68,16 +68,15 @@ module.exports = class Template {
 
         // do this *after* it is parsed, otherwise the properties
         // will be removed...
-        const parts = path.basename(fileName, ".json").split("-");
+        const parts = path.basename(fileName, '.json').split('-');
 
         if (parts && parts.length === 3) {
-            this.id = parts[2].replace(".json", "");
+            this.id = parts[2].replace('.json', '');
             this.type = parts[1];
-        }
-        else {
-            const err = new Error("Invalid template type");
+        } else {
+            const err = new Error('Invalid template type');
 
-            err.code = "INVALID_TEMPLATE_TYPE";
+            err.code = 'INVALID_TEMPLATE_TYPE';
             err.details = fileName;
 
             throw err;
@@ -85,7 +84,7 @@ module.exports = class Template {
     }
 
     parse(str) {
-        assert.strictEqual(typeof str, "string", "str is required");
+        assert.strictEqual(typeof str, 'string', 'str is required');
 
         if (!this.empty()) {
             const props = Object.keys(this);
@@ -95,8 +94,8 @@ module.exports = class Template {
             }
         }
 
-        this.id = "(string)";
-        this.type = "n/a";
+        this.id = '(string)';
+        this.type = 'n/a';
 
         const obj = JSON.parse(str);
         const keys = Object.keys(obj);
@@ -107,20 +106,19 @@ module.exports = class Template {
     }
 
     save(fileName, force) {
-        assert.strictEqual(typeof fileName, "string", "fileName is required");
-        force = force || false;  //eslint-disable-line no-param-reassign
+        fileName = fileName || path.resolve(path.join(cosi.reg_dir, `template-${this.type}-${this.id}.json`)); // eslint-disable-line no-param-reassign
+        force = force || false;  // eslint-disable-line no-param-reassign
 
         const options = {
-            encoding: "utf8",
+            encoding: 'utf8',
             mode: 0o644,
-            flag: force ? "w" : "wx"
+            flag: force ? 'w' : 'wx'
         };
 
         try {
             fs.writeFileSync(fileName, JSON.stringify(this, templatePropertyFilter, 4), options);
-        }
-        catch (err) {
-            if (err.code === "EEXIST") {
+        } catch (err) {
+            if (err.code === 'EEXIST') {
                 console.error(chalk.red(`Template already exists, use --force to overwrite. '${fileName}'`));
                 process.exit(1);
             }
