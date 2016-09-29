fb997f0f02bcdf764fe6a5060dd59180967ff268
matt maier <maier@users.noreply.github.com>
2016-09-29 16:44:37 -0400

    verify each plugin script enabled is present in nad metric output before continuing

--- src/util/lib/cosi/plugins/postgres/nad-enable.sh
+++ src/util/lib/cosi/plugins/postgres/nad-enable.sh
@@ -14,11 +14,32 @@ fi
 # pull in the plugin settings
 source /opt/circonus/etc/pg-conf.sh
 
+pg_scripts=""
+read -r -d '' pg_scripts <<-2f17fc42839fca05a41430b091f087e2
+pg_bgwriter.sh
+pg_cache.sh
+pg_connections.sh
+pg_db_size.sh
+pg_isready.sh
+pg_locks.sh
+pg_partitions.sh
+pg_protocol_observer.sh
+pg_repl_lag.sh
+pg_repl_slots.sh
+pg_replication.sh
+pg_table_stats.sh
+pg_transactions.sh
+pg_vacuum.sh
+2f17fc42839fca05a41430b091f087e2
+enabled_scripts=()
+
 # turn on all postgres stuff (create symlink if it doesn't already exist)
 pushd $NAD_SCRIPTS_DIR >/dev/null
-for i in $PG_SCRIPTS_DIR/*; do
-    ib=$(basename $i)
-    [[ -h $ib ]] || ln -s $i .
+for script in $pg_scripts; do
+    if [[ -x $PG_SCRIPTS_DIR/$script ]]; then
+        [[ -h $script ]] || ln -s $script .
+        enabled_scripts+=(${script%.*})
+    fi
 done
 
 # execute one of the scripts to ensure correct functionality
@@ -49,22 +70,27 @@ fi
 
 popd >/dev/null
 
-# ensure nad is exposing some of the new metrics
+expected=${#enabled_scripts[@]}
 found=0
 for i in {0..10}; do
-    res=$(curl -sS localhost:2609/run/pg_db_size | grep -c '_value')
-    if [[ $res -gt 0 ]]; then
-        found=1
-        break
+    res=$(curl -sS localhost:2609/)
+    for x in ${enabled_scripts[*]}; do
+       	has=$(echo $res | grep -c $x)
+       	if [[ $has -gt 0 ]]; then
+       		((found++))
+       	fi
+    done
+    if [[ $found -eq $expected ]]; then
+       		break;
     fi
-    sleep 1
+    sleep 3
 done
 
-if [[ found -eq 0 ]]; then
+if [[ $found -ne $expected ]]; then
     echo "{\"enabled\": false }"
     exit 1
 fi
 
 # if we have arrived here, the postgres plugin in NAD is installed and operating
-echo "{\"enabled\": true, \"fs_mount\": \"${FS_NAME}\", \"data_dir\": \"${DATA_DIR}\", \"protocol_observer\": ${PROTOCOL_OBSERVER} }"
+echo "{\"enabled\": true, \"fs_mount\": \"${FS_NAME}\", \"data_dir\": \"${DATA_DIR}\", \"protocol_observer\": ${PROTOCOL_OBSERVER}, \"scripts\": \"${enabled_scripts[*]}\" }"
 exit 0
