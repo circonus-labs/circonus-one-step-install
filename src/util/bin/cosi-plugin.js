bb2b3e867e6b710f06645c68e790b2b067ef9c5f
matt maier <maier@users.noreply.github.com>
2016-10-03 14:14:29 -0400

    add cassandra plugin

--- src/util/bin/cosi-plugin.js
+++ src/util/bin/cosi-plugin.js
@@ -12,6 +12,7 @@ const cosi = require(path.resolve(path.join(__dirname, '..', 'lib', 'cosi')));
 
 app.
     version(cosi.app_version).
+    command('cassandra', 'Manage Cassandra plugin').
     command('postgres', 'Manage PostgreSQL plugin').
     parse(process.argv);
 
