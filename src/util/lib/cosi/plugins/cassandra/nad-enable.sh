7ff86ce166c24f4e9cdb75fdd8968256e024b026
matt maier <maier@users.noreply.github.com>
2016-10-03 15:52:50 -0400

    minor updates

--- src/util/lib/cosi/plugins/cassandra/nad-enable.sh
+++ src/util/lib/cosi/plugins/cassandra/nad-enable.sh
@@ -4,63 +4,73 @@
 NAD_SCRIPTS_DIR=/opt/circonus/etc/node-agent.d
 CASS_SCRIPTS_DIR=$NAD_SCRIPTS_DIR/cassandra
 
-# determine if postgres is running
+# determine if cassandra is running
 CASSREL=`nodetool version 2>/dev/null | grep ReleaseVersion`
 if [ -z "$CASSREL" ]; then
     echo "Cassandra not detected, skipping setup"
     exit 1;
 fi
 
-CASSCLUSTER=`nodetool describecluster 2>/dev/null | grep Name | awk '{$1 = ""; print substr($0,2)}'`
+CASSCLUSTER=$(nodetool describecluster 2>/dev/null | awk 'BEGIN{FS=":"}/Name:/{gsub(/^[ ]+/,"",$2);print $2}')
+
+cass_scripts=""
+read -r -d ' ' cass_scripts <<-f22a9a7b7066c7ed6486f71e6ac66e79
+cassandra_cfstats.sh
+cassandra_compaction.sh
+cassandra_gcstats.sh
+cassandra_info.sh
+cassandra_protocol_observer.sh
+f22a9a7b7066c7ed6486f71e6ac66e79
+enabled_scripts=()
 
 # turn on all cassandra stuff
 pushd $NAD_SCRIPTS_DIR >/dev/null
-for i in $CASS_SCRIPTS_DIR/*; do
-    ln -s $i .
+for script in $cass_scripts; do
+    if [[ -x $CASS_SCRIPTS_DIR/$script ]]; then
+        [[ -h $script ]] || ln -s $CASS_SCRIPTS_DIR/$script
+        enabled_scripts+=(${script%.*})
+    fi
 done
 
 # execute one of the scripts to ensure it's working
 SECS=`./cassandra_info.sh | grep uptime_secs`
 if [ -z "$SECS" ]; then
     echo "Could not execute a test script.  Is nodetool and gawk in your path"
-    for i in $CASS_SCRIPTS_DIR/*; do
-        rm `basename $i`
-    done    
-    echo "{\"enabled\": false}"    
+    for script in $cass_scripts; do
+        [[ -h $script ]] && rm $script
+    done
+    echo "{\"enabled\": false}"
     exit 1
 fi
+popd > /dev/null
 
-POPATH=`which protocol_observer`
+# default protocol_observer location
+po=/opt/circonus/bin/protocol_observer
+[[ -x $po ]] || po=`type -P protocol_observer`
 PROTOCOL_OBSERVER="false"
-if [ -n "$POPATH" ]; then
+if [ -n "$po" ]; then
   PROTOCOL_OBSERVER="true"
 fi
 
-sleep 3
-
-# ensure nad is exposing some of the new metrics
+# ensure nad is exposing plugin scripts
+expected=${#enabled_scripts[@]}
 found=0
 for i in {0..10}; do
-    res=$(curl -sS localhost:2609 | grep -c 'cassandra_info')
-    if [[ $res -gt 0 ]]; then
-        found=1
-        break
-    fi
-    sleep 1
+    res=$(curl -sS localhost:2609/)
+    for x in ${enabled_scripts[*]}; do
+        has=$(echo $res | grep -c $x)
+        [[ $has -gt 0 ]] && ((found++))
+    done
+    [[ $found -eq $expected ]] && break
+    sleep 3
 done
 
-if [[ found -eq 0 ]]; then
+if [[ $found -ne $expected ]]; then
     echo "{\"enabled\": false }"
     exit 1
 fi
 
+echo "{\"enabled\": true, \"protocol_observer\": ${PROTOCOL_OBSERVER}, \"cluster_name\": \"${CASSCLUSTER}\", \"scripts\": \"${enabled_scripts[*]}\" }"
 
-# find where the cassandra data is stored
-DATA_DIR=""
-DATA_DIR="$(echo -e "${DATA_DIR}" | tr -d '[[:space:]]')"
-echo "{\"enabled\": true, \"data_dir\": \"${DATA_DIR}\", \"protocol_observer\": ${PROTOCOL_OBSERVER}, \"cluster_name\": \"${CASSCLUSTER}\" }"
-
-popd >/dev/null
-
-# if we have arrived here, the cassandra plugin in NAD is installed and operating 
+# if we have arrived here, the cassandra plugin in NAD is installed and operating
 exit 0
