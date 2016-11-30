4481950e9653b05830d58857ec8bc41536446db0
matt maier <maier@users.noreply.github.com>
2016-11-30 09:13:44 -0500

    add: nad_etc_dir

--- src/util/lib/cosi/index.js
+++ src/util/lib/cosi/index.js
@@ -38,6 +38,7 @@ class COSI {
         instance.log_dir = path.resolve(path.join(instance.cosi_dir, 'log'));
         instance.reg_dir = path.resolve(path.join(instance.cosi_dir, 'registration'));
         instance.ruleset_dir = path.resolve(path.join(instance.cosi_dir, 'rulesets'));
+        instance.nad_etc_dir = path.resolve(path.join(instance.cosi_dir, '..', 'etc'));
 
         const configFile = path.resolve(path.join(instance.etc_dir, 'cosi.json'));
         let cfg = {};
