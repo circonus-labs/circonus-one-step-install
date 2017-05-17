3e464490b18234846bf53cd5599aab6400ff9a5e
matt maier <maier@users.noreply.github.com>
2017-05-17 16:16:26 -0400

    adhere to new lint rules

--- src/lib/templates/index.js
+++ src/lib/templates/index.js
@@ -1,26 +1,26 @@
-/*eslint-env node, es6 */
-/*eslint-disable global-require, no-magic-numbers */
-
-"use strict";
+'use strict';
 
 // load core modules
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 // load app modules
-const settings = require(path.normalize(path.join(__dirname, "..", "settings")));
-const log = require(path.normalize(path.join(__dirname, "..", "logger")));
+const settings = require(path.normalize(path.join(__dirname, '..', 'settings')));
+const log = require(path.normalize(path.join(__dirname, '..', 'logger')));
 
 let instance = null;
 
 class Templates {
 
+    /**
+     * initialize templates object
+     */
     constructor() {
         if (instance !== null) {
             return instance;
         }
 
-        instance = this; //eslint-disable-line consistent-this
+        instance = this; // eslint-disable-line consistent-this
 
         this.load_time = new Date();
 
@@ -37,30 +37,38 @@ class Templates {
         return instance;
     }
 
-
-    get(templateCategory, templateName, dist, vers, arch) {
+    /**
+     * get specific template
+     * @arg {String} templateCategory check, graph, etc.
+     * @arg {String} templateName system, vm, cpu, etc.
+     * @arg {String} dist os distribution (templates can be specific)
+     * @arg {String} vers os distribution version (templates can be specific)
+     * @arg {String} arch platform architecture (templates can be specific)
+     * @returns {Object} template or null
+     */
+    get(templateCategory, templateName, dist, vers, arch) { // eslint-disable-line max-params
         const templateFileName = `${templateCategory}-${templateName}.json`;
         const defaultFileList = [
             {
-                key: this._templateKey(templateCategory, templateName),
-                file: path.resolve(path.join(this.dir, templateFileName))
+                file : path.resolve(path.join(this.dir, templateFileName)),
+                key  : this._templateKey(templateCategory, templateName)
             }
         ];
 
         if (dist) {
             defaultFileList.unshift({
-                key: this._templateKey(templateCategory, templateName, dist),
-                file: path.resolve(path.join(this.dir, dist, templateFileName))
+                file : path.resolve(path.join(this.dir, dist, templateFileName)),
+                key  : this._templateKey(templateCategory, templateName, dist)
             });
             if (vers) {
                 defaultFileList.unshift({
-                    key: this._templateKey(templateCategory, templateName, dist, vers),
-                    file: path.resolve(path.join(this.dir, dist, vers, templateFileName))
+                    file : path.resolve(path.join(this.dir, dist, vers, templateFileName)),
+                    key  : this._templateKey(templateCategory, templateName, dist, vers)
                 });
                 if (arch) {
                     defaultFileList.unshift({
-                        key: this._templateKey(templateCategory, templateName, dist, vers, arch),
-                        file: path.resolve(path.join(this.dir, dist, vers, arch, templateFileName))
+                        file : path.resolve(path.join(this.dir, dist, vers, arch, templateFileName)), // eslint-disable-line max-len
+                        key  : this._templateKey(templateCategory, templateName, dist, vers, arch)
                     });
                 }
             }
@@ -72,17 +80,15 @@ class Templates {
             }
 
             try {
-                const template = require(templateDefinition.file);
+                const template = require(templateDefinition.file); // eslint-disable-line global-require
 
                 if (settings.cache_templates) {
                     this.cache.set(templateDefinition.key, template);
                 }
 
                 return template;
-
-            }
-            catch (err) {
-                if (err.code !== "MODULE_NOT_FOUND") {
+            } catch (err) {
+                if (err.code !== 'MODULE_NOT_FOUND') {
                     log.warn(`Template found but, unable to load: ${templateDefinition.file} ${err}`);
                 }
             }
@@ -92,6 +98,13 @@ class Templates {
     }
 
 
+    /**
+     * list templates
+     * @arg {String} dist os distribution (templates can be specific)
+     * @arg {String} vers os distribution version (templates can be specific)
+     * @arg {String} arch platform architecture (templates can be specific)
+     * @returns {Object} templates or null
+     */
     list(dist, vers, arch) {
         const result = {};
 
@@ -117,6 +130,13 @@ class Templates {
     }
 
 
+    /**
+     * list templates from specific dir
+     * @arg {String} dist os distribution (templates can be specific)
+     * @arg {String} vers os distribution version (templates can be specific)
+     * @arg {String} arch platform architecture (templates can be specific)
+     * @returns {Object} templates or null
+     */
     _listDir(dist, vers, arch) {
         const dirs = [ this.dir ];
 
@@ -136,41 +156,41 @@ class Templates {
         const result = {};
 
         try {
-            const files = fs.readdirSync(dir);
+            const files = fs.readdirSync(dir); // eslint-disable-line no-sync
 
             for (const file of files) {
-                result[file.replace(".json", "")] = path.resolve(dir, file);
+                result[file.replace('.json', '')] = path.resolve(dir, file);
             }
-        }
-        catch (err) {
-            if (err.code !== "ENOENT") {
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
                 log.info(err.message);
             }
         }
 
         return result;
-
     }
 
 
+    /**
+     * load default templates
+     * @returns {Object} list of default templates
+     */
     _loadDefaultTemplates() {
-        const files = fs.readdirSync(this.dir);
+        const files = fs.readdirSync(this.dir); // eslint-disable-line no-sync
 
-        log.info("Loading default templates");
+        log.info('Loading default templates');
 
         for (const file of files) {
-            const matches = file.match(/^([a-z]+)\-([a-z_]+)\.json$/);
+            const matches = file.match(/^([a-z]+)-([a-z_]+).json$/);
 
             if (matches && matches.length === 3) {
-
                 const templateCategory = matches[1];
                 const templateName = matches[2];
                 let template = null;
 
                 try {
-                    template = require(path.resolve(path.join(this.dir, file)));
-                }
-                catch (err) {
+                    template = require(path.resolve(path.join(this.dir, file))); // eslint-disable-line global-require
+                } catch (err) {
                     log.error(`Unable to load template ${file} ${err}`);
                     continue;
                 }
@@ -184,8 +204,17 @@ class Templates {
     }
 
 
-    _templateKey(templateCategory, templateName, dist, vers, arch) {
-        return `${dist || ""}|${vers || ""}|${arch || ""}|${templateCategory}-${templateName}`;
+    /**
+     * generate template key
+     * @arg {String} templateCategory check, graph, etc.
+     * @arg {String} templateName system, vm, cpu, etc.
+     * @arg {String} dist os distribution (templates can be specific)
+     * @arg {String} vers os distribution version (templates can be specific)
+     * @arg {String} arch platform architecture (templates can be specific)
+     * @returns {String} key
+     */
+    _templateKey(templateCategory, templateName, dist, vers, arch) { // eslint-disable-line class-methods-use-this, max-params
+        return `${dist || ''}|${vers || ''}|${arch || ''}|${templateCategory}-${templateName}`;
     }
 
 }
