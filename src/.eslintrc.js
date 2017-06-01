53c84b279c01fa26df4537855eebad23ef77279f
matt maier <maier@users.noreply.github.com>
2017-06-01 08:45:35 -0400

    upd pkg names for eslint configs

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     root: true,
     extends: [
-        '@maier/base',
-        '@maier/node'
+        '@maier/eslint-config-base',
+        '@maier/eslint-config-node'
     ],
     rules: {
         'max-len': [
